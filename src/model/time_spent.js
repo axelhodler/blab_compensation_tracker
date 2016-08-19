6dfc035c869bc82d0d0e75b0be6e2d1f4e7876aa
Axel Hodler <derxor@gmail.com>
2016-08-19 18:28:23 +0200

    mv validation to verification

--- src/model/time_spent.js
+++ src/model/time_spent.js
@@ -1,15 +1,19 @@
 function TimeSpent(members, memberId) {
   this._verificationCount = 0;
   this._memberId = memberId;
+  this._isValid = false;
 
   this.verify = function(memberId) {
     if (this._memberId !== memberId) {
       this._verificationCount++;
+      if (this._verificationCount === members.requiredMajority()) {
+        this._isValid = true;
+      }
     }
   };
 
   this.isValid = function() {
-    return this._verificationCount === members.requiredMajority() ? true : false;
+    return this._isValid;
   };
 }
 
