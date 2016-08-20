adccdff251f30df0e51907905ac62f4580c7f262
Axel Hodler <derxor@gmail.com>
2016-08-20 20:24:00 +0200

    prepare verified reports list feature

--- features/step_definitions/list_unverified_report.js
+++ features/step_definitions/list_unverified_report.js
@@ -11,6 +11,12 @@ module.exports = function() {
     reports.add(new Report(reportId, memberId));
   });
 
+  this.Given(/^a verified report "([^"]*)" by member (\d+)$/, function (reportId, memberId) {
+    var report = new Report(reportId, memberId);
+    report.makeValid();
+    reports.add(report);
+  });
+
   this.When(/^fetching a list of unverified reports$/, function() {
     listOfReports = reports.list();
   });
@@ -21,6 +27,10 @@ module.exports = function() {
 
   this.When(/^fetch unverified report "([^"]*)"$/, function(reportId) {
     report = reports.fetch(reportId);
+  });
+
+  this.When(/^fetching a list of verified reports$/, function() {
+    listOfReports = reports.verified();
   });
 
   this.Then(/^the list consists of the report "([^"]*)" by member (\d+)$/, function(reportId, memberId) {
