5265a400730ef387e805cdc0254957d142e0ccd0
Axel Hodler <derxor@gmail.com>
2016-08-21 16:29:07 +0200

    vars are visible if providing via rest

--- src/model/report.js
+++ src/model/report.js
@@ -1,43 +1,43 @@
 function Report(reportId, memberId) {
-  this._reportId = reportId;
-  this._memberId = memberId;
-  this._isValid = false;
-  this._membersHavingVerified = [];
+  this.reportId = reportId;
+  this.memberId = memberId;
+  this.valid = false;
+  this.verifiers = [];
 
   this.id = function() {
-    return this._reportId;
+    return this.reportId;
   };
 
   this.verifyBy = function(verifierId) {
     if (notTryingToSelfValidate.call(this, verifierId)) {
       if (hasNotVerifiedYet.call(this, verifierId)) {
-        this._membersHavingVerified.push(verifierId);
+        this.verifiers.push(verifierId);
       }
     }
   };
 
   this.hasReceivedEnoughVerifications = function(requiredMajority) {
-    return this._membersHavingVerified.length === requiredMajority;
+    return this.verifiers.length === requiredMajority;
   };
 
   this.submitter = function() {
-    return this._memberId;
+    return this.memberId;
   };
 
   this.makeValid = function() {
-    this._isValid = true;
+    this.valid = true;
   };
 
   this.isValid = function() {
-    return this._isValid;
+    return this.valid;
   };
 
   function notTryingToSelfValidate(verifierId) {
-    return this._memberId !== verifierId;
+    return this.memberId !== verifierId;
   }
 
   function hasNotVerifiedYet(verifierId) {
-    return this._membersHavingVerified.indexOf(verifierId) === -1;
+    return this.verifiers.indexOf(verifierId) === -1;
   }
 }
 
