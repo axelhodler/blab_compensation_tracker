554f12f392ed49b3fd4f08e8251b6c5e94f914ba
Axel Hodler <derxor@gmail.com>
2016-09-10 19:25:47 +0200

    remove obsolete code

--- features/report_output.feature
+++ features/report_output.feature
@@ -4,10 +4,6 @@ Feature: Report achieved output
   I want to report the output i have achieved
 
   Scenario: Reporting achieved output
-    Given an input of 1 hours which achieved output of "foo" on date "1473188008163"
-    When its reported by member "1"
-    Then the report itself is hashed to "dd4b25ab407e3c0bdea13d054034773d202b209ea04b87f1f144e859bf6a38b3"
-
     Given an input of 3 hours which achieved output of "something" on date "1473526945806" by member 1
     When fetching a list of unverified reports
     Then the list consists of the report "8019080947503e42c0e9e1747dabf838801f5775a26c0f00ed057cc9c08dc008" by member 1
--- features/step_definitions/report_output.js
+++ features/step_definitions/report_output.js
@@ -1,15 +1,7 @@
 var Hash = require('../../src/actions/hash');
 var Report = require('../../src/model/report');
-var Reports = require('../../src/model/reports');
 
 module.exports = function() {
-  function UserReports() {
-    var reports = [];
-
-    this.add = function(report) {
-      reports.push(report);
-    }
-  }
 
   function UserReport(input, output, date, memberId) {
     this._input = input;
@@ -23,32 +15,9 @@ module.exports = function() {
     }
   }
 
-  var r;
-
   this.Given(/^an input of (\d+) hours which achieved output of "([^"]*)" on date "([^"]*)" by member (\d+)$/, function (hoursSpent, outputDescription, date, memberId) {
-    r = new UserReport(hoursSpent, outputDescription, date, memberId);
+    var r = new UserReport(hoursSpent, outputDescription, date, memberId);
     reports.add(new Report(r.hash(), r._memberId));
   });
 
-  this.Given(/^an input of (\d+) hours which achieved output of "([^"]*)" on date "([^"]*)"$/, function(hoursSpent, outputDescription, date) {
-    r = new UserReport(hoursSpent, outputDescription, date);
-  });
-
-  this.When(/^generating the hash of the report$/, function () {
-  });
-
-  this.Then(/^the hash is "([^"]*)"$/, function(expectedHash) {
-    expect(r.hash()).to.equal(expectedHash);
-  });
-
-  this.When(/^its reported by member "([^"]*)"$/, function(memberId) {
-    var userReports = new UserReports();
-    userReports.add(r);
-  });
-
-  this.Then(/^the report itself is hashed to "([^"]*)"$/, function(expectedHash) {
-    expect(r.hash()).to.equal(expectedHash);
-  });
-
 };
-
